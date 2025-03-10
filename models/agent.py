3b723e3345c46c547cbd5c8376a3af955d803a97
K.D. Sunera Avinash Chandrasiri <kdsuneraavinash@gmail.com>
2025-03-10 13:22:38 +0530

    Fix mypy errors

--- models/agent.py
+++ models/agent.py
@@ -1,11 +1,12 @@
 import torch
 
+from models.base_agent import BaseAgent
 from models.drl_agent import DrlAgent
 from models.gat_agent import GatAgent
 from models.gin_agent import GinAgent
 
 
-def make_agent(agent_type: str, device: torch.device):
+def make_agent(agent_type: str | None, device: torch.device) -> BaseAgent:
     if agent_type == "gin":
         return GinAgent(device=device)
     elif agent_type == "gat":
