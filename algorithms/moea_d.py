60432c598527ca92b63353c17d104cdb98ddc4d4
K.D. Sunera Avinash Chandrasiri <kdsuneraavinash@gmail.com>
2025-03-22 22:11:56 +0530

    Add MOEA/D and NSGA3 algorithms

--- algorithms/moea_d.py
+++ algorithms/moea_d.py
@@ -1,17 +1,19 @@
 from algorithms.base_mo import BaseMoScheduler, SolutionStoreType
 from algorithms.pymoo_problem import PyMooOptimizationProblem, decode_pymoo_x
 from dataset.models import Dataset
-from pymoo.algorithms.moo.nsga2 import NSGA2
+from pymoo.algorithms.moo.moead import MOEAD
 from pymoo.optimize import minimize
+from pymoo.util.ref_dirs import get_reference_directions
 
 
-class Nsga2Scheduler(BaseMoScheduler):
+class MoeaDScheduler(BaseMoScheduler):
     def __init__(self, store: SolutionStoreType, index: int) -> None:
-        super().__init__("NSGA-II", store, index)
+        super().__init__("MOEA/D", store, index)
 
     def get_pareto_solutions(self, dataset: Dataset) -> list[list[tuple[int, int]]]:
         problem = PyMooOptimizationProblem(dataset)
-        algorithm = NSGA2(pop_size=100)
+        ref_dirs = get_reference_directions("uniform", n_dim=3, n_partitions=12)
+        algorithm = MOEAD(ref_dirs)
         res = minimize(problem, algorithm, termination=("n_gen", 20), seed=1)
 
         return [decode_pymoo_x(dataset, sol) for sol in res.X]  # type: ignore
--- algorithms/nsga_3.py
+++ algorithms/nsga_3.py
@@ -1,17 +1,19 @@
 from algorithms.base_mo import BaseMoScheduler, SolutionStoreType
 from algorithms.pymoo_problem import PyMooOptimizationProblem, decode_pymoo_x
 from dataset.models import Dataset
-from pymoo.algorithms.moo.nsga2 import NSGA2
+from pymoo.algorithms.moo.nsga3 import NSGA3
 from pymoo.optimize import minimize
+from pymoo.util.ref_dirs import get_reference_directions
 
 
-class Nsga2Scheduler(BaseMoScheduler):
+class Nsga3Scheduler(BaseMoScheduler):
     def __init__(self, store: SolutionStoreType, index: int) -> None:
-        super().__init__("NSGA-II", store, index)
+        super().__init__("NSGA-III", store, index)
 
     def get_pareto_solutions(self, dataset: Dataset) -> list[list[tuple[int, int]]]:
         problem = PyMooOptimizationProblem(dataset)
-        algorithm = NSGA2(pop_size=100)
+        ref_dirs = get_reference_directions("uniform", n_dim=3, n_partitions=12)
+        algorithm = NSGA3(pop_size=100, ref_dirs=ref_dirs)
         res = minimize(problem, algorithm, termination=("n_gen", 20), seed=1)
 
         return [decode_pymoo_x(dataset, sol) for sol in res.X]  # type: ignore
